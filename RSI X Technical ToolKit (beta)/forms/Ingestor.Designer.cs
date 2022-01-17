9c50225836632d70998dbdc65abfa257193d1571
HarryP0ster <garryplakat@gmail.com>
2022-01-17 13:35:48 +0800

    .

--- RSI X Technical ToolKit (beta)/forms/Ingestor.Designer.cs
+++ RSI X Technical ToolKit (beta)/forms/Ingestor.Designer.cs
@@ -48,25 +48,25 @@ namespace RSI_X_Desktop.forms
             this.panelRelayButtons.AutoScroll = true;
             this.panelRelayButtons.BackColor = System.Drawing.Color.Silver;
             this.panelRelayButtons.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelRelayButtons.Location = new System.Drawing.Point(0, 81);
+            this.panelRelayButtons.Location = new System.Drawing.Point(0, 61);
             this.panelRelayButtons.Margin = new System.Windows.Forms.Padding(0);
             this.panelRelayButtons.Name = "panelRelayButtons";
-            this.panelRelayButtons.Size = new System.Drawing.Size(675, 650);
+            this.panelRelayButtons.Size = new System.Drawing.Size(590, 488);
             this.panelRelayButtons.TabIndex = 1;
             // 
             // tableLayoutPanel2
             // 
             this.tableLayoutPanel2.ColumnCount = 1;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 99.99999F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel2.Controls.Add(this.label_nameOfConference, 0, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(5, 4);
-            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(4, 3);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(665, 73);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(582, 55);
             this.tableLayoutPanel2.TabIndex = 2;
             // 
             // label_nameOfConference
@@ -79,7 +79,7 @@ namespace RSI_X_Desktop.forms
             this.label_nameOfConference.Location = new System.Drawing.Point(0, 0);
             this.label_nameOfConference.Margin = new System.Windows.Forms.Padding(0);
             this.label_nameOfConference.Name = "label_nameOfConference";
-            this.label_nameOfConference.Size = new System.Drawing.Size(665, 73);
+            this.label_nameOfConference.Size = new System.Drawing.Size(582, 55);
             this.label_nameOfConference.TabIndex = 1;
             this.label_nameOfConference.Text = "Name of conference";
             this.label_nameOfConference.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -93,14 +93,14 @@ namespace RSI_X_Desktop.forms
             this.tableLayoutPanel1.Controls.Add(this.panelRelayButtons, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 27);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 20);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(675, 813);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(590, 610);
             this.tableLayoutPanel1.TabIndex = 11;
             // 
             // tableLayoutPanel3
@@ -113,13 +113,13 @@ namespace RSI_X_Desktop.forms
             this.tableLayoutPanel3.Controls.Add(this.mButton_cancel, 2, 0);
             this.tableLayoutPanel3.Controls.Add(this.mButton_start, 1, 0);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 731);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 549);
             this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 1;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 82F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(675, 82);
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 62F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(590, 61);
             this.tableLayoutPanel3.TabIndex = 4;
             // 
             // mButton_cancel
@@ -132,11 +132,11 @@ namespace RSI_X_Desktop.forms
             this.mButton_cancel.Image = null;
             this.mButton_cancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.mButton_cancel.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
-            this.mButton_cancel.Location = new System.Drawing.Point(551, 9);
-            this.mButton_cancel.Margin = new System.Windows.Forms.Padding(11, 9, 11, 9);
+            this.mButton_cancel.Location = new System.Drawing.Point(482, 7);
+            this.mButton_cancel.Margin = new System.Windows.Forms.Padding(10, 7, 10, 7);
             this.mButton_cancel.Name = "mButton_cancel";
             this.mButton_cancel.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
-            this.mButton_cancel.Size = new System.Drawing.Size(113, 64);
+            this.mButton_cancel.Size = new System.Drawing.Size(98, 47);
             this.mButton_cancel.TabIndex = 81;
             this.mButton_cancel.Text = "Cancel";
             this.mButton_cancel.TextAlignment = System.Drawing.StringAlignment.Center;
@@ -152,11 +152,11 @@ namespace RSI_X_Desktop.forms
             this.mButton_start.Image = null;
             this.mButton_start.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.mButton_start.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
-            this.mButton_start.Location = new System.Drawing.Point(416, 9);
-            this.mButton_start.Margin = new System.Windows.Forms.Padding(11, 9, 11, 9);
+            this.mButton_start.Location = new System.Drawing.Point(364, 7);
+            this.mButton_start.Margin = new System.Windows.Forms.Padding(10, 7, 10, 7);
             this.mButton_start.Name = "mButton_start";
             this.mButton_start.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
-            this.mButton_start.Size = new System.Drawing.Size(113, 64);
+            this.mButton_start.Size = new System.Drawing.Size(98, 47);
             this.mButton_start.TabIndex = 82;
             this.mButton_start.Text = "Start!";
             this.mButton_start.TextAlignment = System.Drawing.StringAlignment.Center;
@@ -170,29 +170,28 @@ namespace RSI_X_Desktop.forms
             this.formTheme1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.formTheme1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
             this.formTheme1.Location = new System.Drawing.Point(0, 0);
-            this.formTheme1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.formTheme1.Name = "formTheme1";
-            this.formTheme1.Padding = new System.Windows.Forms.Padding(3, 27, 3, 27);
+            this.formTheme1.Padding = new System.Windows.Forms.Padding(3, 20, 3, 20);
             this.formTheme1.Sizable = true;
-            this.formTheme1.Size = new System.Drawing.Size(681, 867);
+            this.formTheme1.Size = new System.Drawing.Size(596, 650);
             this.formTheme1.SmartBounds = false;
             this.formTheme1.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.formTheme1.TabIndex = 12;
-            this.formTheme1.Text = "INJECTOR";
+            this.formTheme1.Text = "EXTRACTOR";
             // 
             // Ingestor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(41)))), ((int)(((byte)(50)))));
-            this.ClientSize = new System.Drawing.Size(681, 867);
+            this.ClientSize = new System.Drawing.Size(596, 650);
             this.Controls.Add(this.formTheme1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
-            this.MinimumSize = new System.Drawing.Size(144, 67);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MinimumSize = new System.Drawing.Size(126, 50);
             this.Name = "Ingestor";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "INJECTOR";
+            this.Text = "EXTRACTOR";
             this.TransparencyKey = System.Drawing.Color.Fuchsia;
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Ingestor_FormClosed);
             this.tableLayoutPanel2.ResumeLayout(false);
